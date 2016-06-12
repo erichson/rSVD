ac11264098c03d336541215cf491e5643206d344
Benli11 <nbe@st-andrews.ac.uk>
2016-06-12 15:37:29 -0700

    data sets

---
+++ R/tiger.R
@@ -0,0 +1,24 @@
+#'Tiger
+#'
+#' @description 1600x1200 grayscaled (8 bit [0-255]/255) image.
+#'
+#' @docType data
+#'
+#' @usage data(tiger)
+#'
+#' @format An object of class \code{"rsvd"}.
+#'
+#' @keywords image
+#'
+#' @references S. Taheri (2006). “Panthera tigris altaica”, (Online image)
+#'
+#' @source \href{https://en.wikipedia.org/wiki/File:Siberischer_tiger_de_edit02.jpg}{Wikimedia}
+#'
+#' @examples
+#' library(rsvd)
+#' data(tiger)
+#'
+#' #Display image
+#' image(tiger, col = gray((0:255)/255))
+#'
+"tiger"
