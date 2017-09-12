c5c990fa54874cb7fea05bd522163d2b0a06ce2e
benli11 <erichson@uw.edu>
2017-09-12 13:03:18 -0700

    rpca plot functions

--- R/ggbiplot.R
+++ R/ggbiplot.R
@@ -1,61 +1,83 @@
 #devtools::use_package("ggplot2", type = "Suggests")
-#devtools::use_package("plyr", type = "Suggests")
-#devtools::use_package("scales", type = "Suggests")
-#devtools::use_package("grid", type = "Suggests")
-#library(ggplot2)
-#library(plyr)
-#library(scales)
-#library(grid)
+#library("ggplot2")
 
-#' Biplot for \code{rPCA} using ggplot2
+#' Biplot for \code{rpca} using ggplot2
 #'
-#' @param  rpcaObj          object containing the \code{sdev} component, such as that returned
-#'                          by \code{rpca}
-#' @param  pcs              array_like \cr
-#'                          an array with two values indicating which two PCs should be plotted,
-#'                          by default the first two PCs are used, e.g., \eqn{c(1,2)}. \cr
-#' @param  groups           optional, factor \cr
-#'                          factor indicating groups
-#' @param  ellipse          draw a 1sd data ellipse for each group
-#' @param  alpha.ellipse    alpha transparency for ellipse
-#' @param  loadings         draw arrows for the variables
-#' @param  labels           label variables
-#' @param  ...              arguments passed to or from other methods, see \code{\link[ggplot2]{ggplot}}.
+#' @description Creates a pretty biplot which is showing the individual factor map overlayed by the
+#' variables factor map, i.e, plotting both the principal component scores and directions.
+#'    
+#' @param  rpcaObj        Object returned by the \code{rpca()} function.
+#'                          
+#' @param  pcs            Array_like. \cr
+#'                        An array with two values indicating the two PCs which should be used for plotting. 
+#'                        By default the first two PCs are used, e.g., \eqn{c(1,2)}. 
 #'
+#' @param loadings   Bool (\eqn{TRUE}, \eqn{FALSE}), optional. \cr
+#'                  If \eqn{TRUE}, the eigenvectors
+#'                  are unit scaled by the square root of the eigenvalues \eqn{W = W * diag(sqrt(eigvals))}.                       
+#'                                                                              
+#' @param  groups         Factor, optional. \cr
+#'                        Factor indicating groups.
+#'                          
+#' @param  alpha          Scalar, optional. \cr
+#'                        Alpha transparency for scatter plot.
+#'                                                                                
+#' @param  ellipse        Bool (\eqn{TRUE}, \eqn{FALSE}), optional. \cr
+#'                        Draw a 1sd data ellipse for each group, if \eqn{TRUE}.
+#'  
+#' @param  alpha.ellipse  Scalar, optional. \cr
+#'                        Alpha transparency for ellipse.
+#' 
+#' @param  var_labels  Bool (\eqn{TRUE}, \eqn{FALSE}), optional. \cr
+#'                        Plot variable names, if \eqn{TRUE}.
+#' 
+#' @param var_labels.names     Array_like, optional. \cr
+#'                        User specific labels for the individuals.     
+#' 
+#' @param  ind_labels  Bool (\eqn{TRUE}, \eqn{FALSE}), optional. \cr
+#'                        Plot data point names, if \eqn{TRUE}.
+#' 
+#' @param ind_labels.names     Array_like, optional. \cr
+#'                        User specific labels for data points. 
+#' 
 #' @seealso \code{\link{rpca}}, \code{\link[ggplot2]{ggplot}}
 #'
-#' @author N. Benjamin Erichson, \email{nbe@st-andrews.ac.uk}
+#' @author N. Benjamin Erichson, \email{erichson@uw.edu}
+#' 
 #' @examples
 #' #See ?rpca
 
 #' @export
-ggbiplot <- function( rpcaObj, pcs = c(1,2), groups = NULL, ellipse = TRUE, alpha.ellipse=0.2,
-                      loadings = TRUE, labels=TRUE, ... )
+ggbiplot <- function( rpcaObj, pcs = c(1,2), loadings=TRUE, groups = NULL, alpha = 0.6, 
+                      ellipse = TRUE, alpha.ellipse=0.2,
+                      var_labels=TRUE, var_labels.names=NULL,
+                      ind_labels=TRUE, ind_labels.names=NULL
+                      )
 {
-
+  
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("The package 'ggplot2' is needed for this function to work. Please install it.",
          call. = FALSE)
   }
-
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Check if retx is provided
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   if(is.null(rpcaObj$x)) stop("ggbiplot requires the rotated variables, i.e., set rpca(..., retx = TRUE).")
-
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Check selected pcs
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   stopifnot(length(pcs) == 2)
   if(max(pcs) > ncol(rpcaObj$rotation)) stop("Selected PC is not valid.")
   if(min(pcs) < 1) stop("Selected PC is not valid.")
-
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # dimensions
+  # Dimensions
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   n = nrow(rpcaObj$x)
   p = nrow(rpcaObj$rotation)
-
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Label PCs
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -63,35 +85,46 @@ ggbiplot <- function( rpcaObj, pcs = c(1,2), groups = NULL, ellipse = TRUE, alph
   explained_variance_ratio = round(variance / rpcaObj$var,3) * 100
   PC1 = paste("PC", pcs[1], "(", explained_variance_ratio[pcs[1]]  , "% explained var.)", sep="")
   PC2 = paste("PC", pcs[2], "(", explained_variance_ratio[pcs[2]]  , "% explained var.)", sep="")
-
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Scale principal component scores
-  # No scaling ?
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  Z1 = rpcaObj$x[, pcs[1]] #* (rpcaObj$eigvals**0.5)[pcs[1]]
-  Z2 = rpcaObj$x[, pcs[2]] #* (rpcaObj$eigvals**0.5)[pcs[2]]
-
+  Z1 = rpcaObj$x[, pcs[1]] #* (rpcaObj$eigvals[pcs[1]]**0.5)
+  Z2 = rpcaObj$x[, pcs[2]] #* (rpcaObj$eigvals[pcs[2]]**0.5)
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Scale prinicipal directions i.e., loadings
+  # Create data frame for variables scores
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  dfrotation = rpcaObj$rotation %*%  diag(rpcaObj$eigvals**0.5)
-  dfrotation = rpcaObj$rotation %*%  diag(rpcaObj$eigvals**0.5)
+  dfscores <- data.frame(scores=cbind(Z1,Z2), row.names = 1:n)
+  colnames(dfscores) <- c( 'a', 'b')
+  
+  if(is.null(rownames(rpcaObj$x))) {
+    dfscores$"indName" <- as.character(1:n)
+  } else {
+    dfscores$"indName" <- rownames(rpcaObj$x)
+  }  
+  
+  if(!is.null(ind_labels.names)) dfscores$"indName" <- ind_labels.names
+  
 
-  # Generate circle
-  #theta <- c(seq(-pi, pi, length = 360))
-  #circle <- data.frame(x = cos(theta), y = sin(theta))
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Create data frame
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  dfScores <- data.frame(scores=cbind(Z1,Z2))
-  colnames(dfScores) <- c( 'a', 'b')
-
-  dfrotation <- data.frame(rotation=dfrotation)
-  rownames(dfrotation) <- rownames(rpcaObj$rotation)
-  colnames(dfrotation) <- c( 'a', 'b')
-  dfrotation$"varName" <- rownames(rpcaObj$rotation)
-
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+  # Create data frame for variables
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+  if(loadings==FALSE) rotation = rpcaObj$rotation  
+  if(loadings==TRUE)  rotation = t(t(rpcaObj$rotation) * rpcaObj$eigvals**0.5)
+  
+  dfvariables <- data.frame(rotation=rotation, row.names = 1:p)
+  colnames(dfvariables) <- c( 'a', 'b')
+  
+  if(is.null(rownames(rpcaObj$rotation))) {
+    dfvariables$"varName" <- as.character(1:p)
+  } else {
+    dfvariables$"varName" <- rownames(rpcaObj$rotation)
+  }
+  
+  if(!is.null(var_labels.names)) dfvariables$"varName" <- var_labels.names
+  
+    
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #Workaround for CRAN: Nulling
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -99,59 +132,69 @@ ggbiplot <- function( rpcaObj, pcs = c(1,2), groups = NULL, ellipse = TRUE, alph
   y <- NULL # Setting the variables to NULL first
   a <- NULL # Setting the variables to NULL first
   b <- NULL # Setting the variables to NULL first
-  Class <- NULL # Setting the variables to NULL first
+  class <- NULL # Setting the variables to NULL first
+  indName <- NULL # Setting the variables to NULL first
   varName <- NULL # Setting the variables to NULL first
+  
+  
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Create ggplot2:: ggplot
+  # Scores
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Circle
-  #g <- ggplot2::ggplot( circle , ggplot2::aes( x , y)  )  +
-  #     ggplot2::geom_path( size=1, colour="#9400d3"  )
+  if(is.null(groups)) {
+    dfscores$"class" <- 'subjects'
+  } else{
+    dfscores$"class" <- groups
+  }
+  
+  g <- ggplot2::ggplot(data=dfscores, ggplot2::aes(x = a, y = b, colour = class )) +
+    ggplot2::geom_point(size = 2, alpha = alpha) +
+    ggplot2::theme(legend.position = "none")
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Stat ellipse
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(is.null(groups) == 0 && ellipse==TRUE){
+    g <- g + ggplot2::stat_ellipse( geom = "polygon", alpha = alpha.ellipse,
+                                    ggplot2::aes(fill = class))
+  }
 
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      # Scores
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      if(is.null(groups)) {
-        dfScores$"Class" <- 'Observations'
-      } else{
-        dfScores$"Class" <- groups
-
-      }
-
-      g <- ggplot2::ggplot(data=dfScores, ggplot2::aes(x = a, y = b, colour = Class )) +
-           ggplot2::geom_point(size = 2) +
-           ggplot2::theme(legend.position = "none")
-
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      # stat_ellipse
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      if(is.null(groups) == 0 && ellipse==TRUE){
-      g <- g + ggplot2::stat_ellipse( geom = "polygon", alpha = alpha.ellipse,
-                               ggplot2::aes(fill = Class))
-      }
-
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      # Principal component directions
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      if(loadings==TRUE){
-        g <- g +
-          ggplot2::geom_segment(data = dfrotation,
-                                ggplot2::aes(x = 0, y = 0, xend = a, yend = b),
-                                arrow = grid::arrow(length = grid::unit(1, 'picas')),
-                                color = 'black',  size = 1.3)
-      }
-
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      # Label the variable axes
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      if(labels==TRUE) {
-        g <- g +
-          ggplot2::geom_text(data = dfrotation,
-                             ggplot2::aes(label = varName, x = a, y = b,
-                                          angle = 1, hjust = 1, vjust = 1),
-                             color = 'black', size = 5)
-      }
-  # g <- g + guides(colour=FALSE)
-  g <- g + ggplot2::labs(x = noquote(PC1), y = noquote(PC2)) + ggplot2::guides(colour=FALSE)
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Label data points
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(ind_labels==TRUE) {
+    g <- g + ggplot2::geom_text(data = dfscores,
+                                ggplot2::aes(label = indName, x = a, y = b,
+                                             angle = 0, hjust = 0.1, vjust = 0.1),
+                                color = 'black', size = 4)
+  }    
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Variable factor map
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+  g <- g + ggplot2::geom_segment(data = dfvariables,
+                                 ggplot2::aes(x = 0, y = 0, xend = a, yend = b ),
+                                 arrow = grid::arrow(length = grid::unit(0.5, 'picas')),
+                                 color = '#de2d26' ,  size = 0.5)  
+  
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Label the variable axes
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(var_labels==TRUE) {
+    g <- g + ggplot2::geom_text(data = dfvariables,
+                                ggplot2::aes(label = varName, x = a, y = b,
+                                             angle = 0, hjust = 0.1, vjust = 0.1),
+                                color = '#de2d26', size = 4)
+  }
+  
+  g <- g + ggplot2::ggtitle('Individuals factor map (PCA)')
+  g <- g + ggplot2::xlab(PC1) +  ggplot2::ylab(PC2) 
+  g <- g + ggplot2::guides(colour=FALSE)
+  g <- g + ggplot2::geom_vline(xintercept=0, linetype="dashed", color = "black")  
+  g <- g + ggplot2::geom_hline(yintercept=0, linetype="dashed", color = "black")    
+  g <- g + ggplot2::theme_bw() 
+  g <- g + ggplot2::theme(panel.grid.major = ggplot2::element_blank(), 
+                          panel.grid.minor = ggplot2::element_blank())    
+  
   return(g)
 }
--- R/plots.R
+++ R/plots.R
@@ -1,173 +1,35 @@
-#devtools::use_package("ggplot2")
-#library(ggplot2)
+#devtools::use_package("ggplot2", type = "Suggests")
+#library("ggplot2")
 
 #' @title Screeplot
 #
-#' @description Creates a screeplpot. By default the explained variance is plotted
-#' agaings the number of the principal component.
-#' Alternatively the eigenvalues, explained variance ratio or the cumulative
-#' explained variance ratio can be plotted.
+#' @description Creates a screeplot, variables and individual factor maps to
+#'              summarize the results of the \code{rpca()} function.
 #'
-#' @param x   object containing the \code{sdev} component, such as that returned
-#'            by \code{rpca}
+#' @param x   Object returned by the \code{rpca()} function.
 #'
-#' @param type      str c('var', 'ratio', 'cum', 'eigenvals'), optional \cr
-#'
-#' @param ...     arguments passed to or from other methods, see \code{\link{plot}}.
+#' @param ...     Additional arguments passed to the individual plot functions (see below).
 #'
 #' @param ................. .
 #'
-#' @seealso \code{\link{rpca}}
+#' @seealso \code{\link{ggscreeplot}}, \code{\link{ggcorplot}} , \code{\link{ggindplot}}  
 #'
 #' @examples #
 #'
-
 #'@export
-plot.rpca <- function(x, type = c('var', 'ratio', 'cum', 'eigenvals'), ... ) {
-
-  type <- match.arg(type)
-
-  y <- switch(type,
-              var = x$sdev**2,
-              ratio = x$sdev**2 / x$var,
-              cum = cumsum(x$sdev**2 / x$var ),
-              eigenvals = x$eigvals,
-              stop("Selected plot option is not supported!")
-
-  )
-
-  y.label <- switch(type,
-                    var = 'Explained variance',
-                    ratio = 'Proportion of variance',
-                    cum = 'Cummulative proportion',
-                    eigenvals = 'Eigenvalues',
-                    stop("Selected plot option is not supported!")
-  )
-
-  df <- data.frame(PC = 1:length(x$sdev), y = y)
-
-  graphics::plot.default(df$PC, df$y, xlab ='PCs', ylab=y.label,
-               type = 'b', pch=20, col='red')
-}
-
-
-
-#' @title Pretty Screeplot
-#
-#' @description Creates a pretty screeplpot using \code{\link[ggplot2]{ggplot}}. By default the explained variance is plotted
-#' agaings the number of the principal component.
-#' Alternatively the eigenvalues, explained variance ratio or the cumulative
-#' explained variance ratio can be plotted.
-#'
-#' @param rpcaObj  object containing the \code{sdev} component, such as that returned
-#'                 by \code{rpca}
-#'
-#' @param type   str c('var', 'ratio', 'cum', 'eigenvals'), optional \cr
-#'
-#' @param ................. .
-#'
-#' @seealso \code{\link{rpca}}, \code{\link[ggplot2]{ggplot}}
-#'
-#' @examples #
-#'
-
-#'@export
-ggscreeplot <- function(rpcaObj, type = c('var', 'ratio', 'cum', 'eigenvals')) {
-
+plot.rpca <- function(x, ... ) {
+  
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("The package 'ggplot2' is needed for this function to work. Please install it.",
          call. = FALSE)
-  }
+  } 
+   
+  print(ggscreeplot(x, ... ))
 
+  print(ggcorplot(x, ... ))
 
-  type <- match.arg(type)
+  print(ggindplot(x, ... ))
 
-  y <- switch(type,
-                  var = rpcaObj$sdev**2,
-                  ratio = rpcaObj$sdev**2 / rpcaObj$var,
-                  cum = cumsum(rpcaObj$sdev**2 / rpcaObj$var ),
-                  eigenvals = rpcaObj$eigvals,
-                  stop("Selected plot option is not supported!")
-
-              )
-
-  y.label <- switch(type,
-                    var = 'Explained variance',
-                    ratio = 'Proportion of variance',
-                    cum = 'Cummulative proportion',
-                    eigenvals = 'Eigenvalues',
-                    stop("Selected plot option is not supported!")
-                    )
-
-  df <- data.frame(PC = 1:length(rpcaObj$sdev), y = y)
-
-  #Workaround for CRAN: Nulling
-  PC <- NULL # Setting the variables to NULL first
-
-
-  ggplot2::ggplot(data = df, ggplot2::aes(x = PC, y = y, color = 'red') ) +
-    ggplot2::xlab('PCs') + ggplot2::ylab( y.label ) +
-    ggplot2::geom_point(size=5) + ggplot2::geom_line(size=1.2) + ggplot2::guides(colour=FALSE)
 }
 
 
-
-#' @title Correlation plot
-#
-#' @description Creates a pretty plot which is showing the correlation of
-#'    the original variable with the principal component (PCs).
-#'
-#' @param rpcaObj    object containing the \code{sdev} component, such as that returned
-#'                   by \code{rpca}
-#'
-#' @param pcs   array_like \cr
-#'              an array with two values indicating which two PCs should be plotted,
-#'              by default the first two PCs are used, e.g., \eqn{c(1,2)}. \cr
-#'
-#' @param ................. .
-#'
-#' @seealso \code{\link{rpca}}, \code{\link[ggplot2]{ggplot}}
-#'
-#' @examples #
-#'
-
-
-#' @export
-ggcorplot <- function( rpcaObj, pcs=c(1,2) ) {
-
-  if (!requireNamespace("ggplot2", quietly = TRUE)) {
-    stop("The package 'ggplot2' is needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
-
-
-  # Check selected pcs
-  stopifnot(length(pcs) == 2)
-  if(max(pcs) > ncol(rpcaObj$rotation)) stop("Selected PC is not valid.")
-  if(min(pcs) < 1) stop("Selected PC is not valid.")
-
-  # Select PCs
-  PC1 = paste("PC", pcs[1], sep="")
-  PC2 = paste("PC", pcs[2], sep="")
-
-  # Generate circle
-  theta <- c(seq(-pi, pi, length = 360))
-  circle <- data.frame(x = cos(theta), y = sin(theta))
-
-  # Create data frame
-  df <- data.frame(rpcaObj$rotation[ , pcs], labels=row.names(rpcaObj$rotation))
-  colnames(df) <- c( 'a', 'b', 'labels' )
-
-  #Workaround for CRAN: Nulling
-  x <- NULL # Setting the variables to NULL first
-  y <- NULL # Setting the variables to NULL first
-  a <- NULL # Setting the variables to NULL first
-  b <- NULL # Setting the variables to NULL first
-
-  # Create ggplot2:: ggplot
-  g <- ggplot2::ggplot( circle , ggplot2::aes( x , y)  )  + ggplot2::geom_path( size=1, colour="#9400d3"  )
-  g <- g + ggplot2::geom_point(data=df, size = 6,  mapping = ggplot2::aes(x = a, y = b, label = labels, colour = labels ) ) +
-    ggplot2::coord_fixed(ratio=1) + ggplot2::labs(x = noquote(PC1), y = noquote(PC2)) + ggplot2::guides(colour=ggplot2::guide_legend(title=NULL))
-
-  return( g )
-}
