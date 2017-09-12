1d014b318b714285ee2480c99c932803bb273271
benli11 <erichson@uw.edu>
2017-09-11 18:26:13 -0700

    QB decomposition

---
+++ R/rqb.R
@@ -0,0 +1,166 @@
+#' @title  Randomized QB Decomposition (rqb).
+#
+#' @description Compute the near-optimal QB decompositon of a rectangular matrix.
+#
+#' @details
+#' The randomized QB decompositon factors a rectangular \eqn{(m,n)} matrix \eqn{A} as
+#' \eqn{A = Q * B}. \eqn{Q} is an \eqn{(m,k)} matrix with orthogonal columns, and \eqn{B} a \eqn{(k,n)} matrix.
+#' The target rank is assumend to be \eqn{k << min(m,n)}.   
+#'
+#' \eqn{p} is an oversampling parameter to improve the approximation.
+#' A value between 5 and 10 is recommended, and \eqn{p=10} is set by default.
+#'
+#' The parameter \eqn{q} specifies the number of power (subspace) iterations
+#' to reduce the approximation error. This is recommended
+#' if the the singular values decay slowly. In practice 1 or 2 iterations
+#' achieve good results, however, computing power iterations increases the
+#' computational time. The number of power iterations is set to \eqn{q=2} by default.
+#'
+#'
+#' @param A       Array_like. \cr
+#'                A real/complex input matrix (or data frame), with dimensions \eqn{(m, n)}.
+#'
+#' @param k       Int, optional. \cr
+#'                Determines the target rank of the low-rank decomposition. It should satisfy \eqn{k << min(m,n)}.
+#'
+#' @param p       Int, optional. \cr
+#'                Oversampling parameter for (default \eqn{p=10}).
+#'
+#' @param q       Int, optional. \cr
+#'                Number of power iterations (default \eqn{q=2}).
+#'
+#' @param sdist   String \eqn{c( 'unif', 'normal', 'rademacher')}, optional. \cr
+#'                Specifies the sampling distribution. \cr
+#'                \eqn{'unif'} :  Uniform `[-1,1]`. \cr
+#'                \eqn{'normal}' (default) : Normal `~N(0,1)`. \cr
+#'                \eqn{'rademacher'} : Rademacher random variates. \cr
+#'
+#' @param rand  Bool (\eqn{TRUE}, \eqn{FALSE}). \cr
+#'              If (\eqn{TRUE}), a probabilistic strategy is used, otherwise a deterministic algorithm is used.
+#'
+#' @param ............. .
+#'
+#'
+#'@return \code{rqb} returns a list containing the following three components:
+#'\item{Q}{  Array_like. \cr
+#'           Orthogonal matrix; array with dimensions \eqn{(m, k)}.
+#'}
+#'
+#'\item{B}{  Array_like. \cr
+#'           Smaller matrix; array with dimensions \eqn{(k, n)}.
+#'}
+#'\item{.............}{.}
+#'
+#' @references
+#' \itemize{
+#'   \item  [1] N. Halko, P. Martinsson, and J. Tropp.
+#'          "Finding structure with randomness: probabilistic
+#'          algorithms for constructing approximate matrix
+#'          decompositions" (2009).
+#'          (available at arXiv \url{http://arxiv.org/abs/0909.4061}).
+#'   \item  [2] S. Voronin and P.Martinsson.
+#'          "RSVDPACK: Subroutines for computing partial singular value
+#'          decompositions via randomized sampling on single core, multi core,
+#'          and GPU architectures" (2015).
+#'          (available at `arXiv \url{http://arxiv.org/abs/1502.05366}).
+#' }
+#'
+#' @author N. Benjamin Erichson, \email{erichson@uw.edu}
+#' 
+#' @seealso \code{\link{svd}}
+#' 
+#'
+
+#' @export
+rqb <- function(A, k=NULL, p=10, q=2, sdist="normal", rand = TRUE) UseMethod("rqb")
+
+#' @export
+rqb.default <- function(A, k=NULL, p=10, q=2, sdist="normal", rand = TRUE) {
+
+    A <- as.matrix(A)
+
+    # Create rqb object
+    rqbObj <- list()
+    
+    #Dim of input matrix
+    m <- nrow(A)
+    n <- ncol(A)
+
+    #Set target rank
+    if(is.null(k)) k = n
+    if(k > n) k <- n
+    if(is.character(k)) stop("Target rank is not valid!")
+    if(k < 1) stop("Target rank is not valid!")
+
+    #Set oversampling parameter
+    l <- round(k) + round(p)
+    if(l > n) l <- n
+    if(l < 1) stop("Target rank is not valid!")
+
+    #Check if array is real or complex
+    if(is.complex(A)) {
+      isreal <- FALSE
+    } else {
+      isreal <- TRUE
+    }
+
+    if(rand == TRUE) {
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #Generate a random sampling matrix O
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        O <- switch(sdist,
+                    normal = matrix(stats::rnorm(l*n), n, l),
+                    unif = matrix(stats::runif(l*n), n, l),
+                    rademacher = matrix(sample(c(-1,1), (l*n), replace = TRUE, prob = c(0.5,0.5)), n, l),
+                    stop("Selected sampling distribution is not supported!"))
+    
+        if(isreal==FALSE) {
+          O <- O + switch(sdist,
+                          normal = 1i * matrix(stats::rnorm(l*n), n, l),
+                          unif = 1i * matrix(stats::runif(l*n), n, l),
+                          rademacher = 1i * matrix(sample(c(-1,1), (l*n), replace = TRUE, prob = c(0.5,0.5)), n, l),
+                          stop("Selected sampling distribution is not supported!"))
+        }
+    
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #Build sample matrix Y : Y = A * O
+        #Note: Y should approximate the range of A
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Y <- A %*% O
+        remove(O)
+    
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #Orthogonalize Y using economic QR decomposition: Y=QR
+        #If q > 0 perfrom q subspace iterations
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        if( q > 0 ) {
+            for( i in 1:q) {
+                Y <- qr.Q( qr(Y, complete = FALSE) , complete = FALSE )
+                Z <- crossprod_help(A , Y )
+                Z <- qr.Q( qr(Z, complete = FALSE) , complete = FALSE )
+                Y <- A %*% Z
+            }#End for
+            remove(Z)
+        }#End if
+
+        rqbObj$Q <- qr.Q( qr(Y, complete = FALSE) , complete = FALSE )
+    }else{
+        rqbObj$Q <- qr.Q( qr(A, complete = FALSE) , complete = FALSE )
+    }
+      
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #Project the data matrix a into a lower dimensional subspace
+    #B := Q.T * A
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    rqbObj$B <- crossprod_help(rqbObj$Q , A )
+
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #Return
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    class(rqbObj) <- "rqb"
+    return(rqbObj) 
+    
+} # End rqb
+
