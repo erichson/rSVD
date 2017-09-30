df7fa228c8ee09246dad072f1f53648b26ef4212
benli11 <erichson@uw.edu>
2017-09-30 16:06:41 -0700

    improved implementation

--- R/rpca.R
+++ R/rpca.R
@@ -1,95 +1,78 @@
 #' @title  Randomized principal component analysis (rpca).
 #
-#' @description Principal components analysis using the randomized singular value decomposition.
+#' @description Fast computation of the principal components analysis using the randomized singular value decomposition.
 #
 #' @details
-#' Principal component analysis is a linear dimensionality reduction technique,
-#' aiming to keep only the most significant principal components to allow
-#' a better interpretation of the data and to project the data to a lower dimensional space.
+#' Principal component analysis is an important linear dimension reduction technique.
 #'
-#' Traditionally, the computation is done by a (deterministic) singular value decomposition.
-#' Randomized PCA is computed using a fast randomized algorithm (\code{\link{rsvd}})
-#' to compute the approximate low-rank SVD decomposition.
-#' The computational gain is high if the desired number of principal components
-#' is small, i.e. \eqn{k << min(m,n)}.
-#'
-#' \code{\link{rsvd}} expects a numeric (real/complex) input matrix with dimensions \eqn{(m, n)}.
-#' Given a target rank \eqn{k}, \code{rsvd} factors the input matrix \eqn{A} as
-#' \eqn{A = W * diag(s) * W'}. The columns of the real or complex unitary matrix \eqn{W}
-#' contain the eigenvectors (i.e. principal components). The vector \eqn{s} contains the corresponding
-#' eigenvalues. Following \code{\link{prcomp}} we denote this matrix \eqn{W} as
-#' rotation matrix (commonly also called loadings).
+#' Randomized PCA is computed via the randomized SVD algorithm (\code{\link{rsvd}}).
+#' The computational gain is substantial, if the desired number of principal components
+#' is relatively small, i.e. \eqn{k << min(m,n)}.
 #'
 #' The print and summary method can be used to present the results in a nice format.
-#' A scree plot can be produced with
-#' \code{\link{ggscreeplot}}. The individuals factor map can be produced with \code{\link{ggindplot}},
+#' A scree plot can be produced with \code{\link{ggscreeplot}}. 
+#' The individuals factor map can be produced with \code{\link{ggindplot}},
 #' and a correlation plot with \code{\link{ggcorplot}}.
 #'
 #' The predict function can be used to compute the scores of new observations. The data
-#' will automatically be centred (and scaled if requested). This is not fully supported for
+#' will automatically be centered (and scaled if requested). This is not fully supported for
 #' complex input matrices.
 #'
 #'
-#' @param A       Array_like. \cr
-#'                A numeric input matrix (or data frame), with dimensions \eqn{(m, n)}. \cr
-#'                If the data contain \eqn{NA}s na.omit is applied.
+#' @param A      array_like; \cr
+#'               a numeric \eqn{(m, n)} input matrix (or data frame) to be analyzed. \cr
+#'               If the data contain \eqn{NA}s na.omit is applied.
 #'
-#' @param k       Int, optional. \cr
-#'                Determines the number of principle components to compute. It is required that \eqn{k} is smaller or equal to
-#'                \eqn{n}, but it is recommended that \eqn{k << min(m,n)}.
+#' @param k      integer; \cr
+#'               number of dominant principle components to be computed. It is required that \eqn{k} is smaller or equal to
+#'               \eqn{min(m,n)}, but it is recommended that \eqn{k << min(m,n)}.
 #'
-#' @param center  Bool (\eqn{TRUE}, \eqn{FALSE}), optional. \cr
-#'                A logical value (\eqn{TRUE} by default) indicating whether the variables should be
-#'                shifted to be zero centered. Alternatively, a vector of length equal the number of
-#'                columns of \eqn{A} can be supplied. The value is passed to scale.
+#' @param center  bool, optional; \cr
+#'                logical value which indicates whether the variables should be
+#'                shifted to be zero centered (\eqn{TRUE} by default).
 #'
-#' @param scale   Bool (\eqn{TRUE}, \eqn{FALSE}), optional. \cr
-#'                A logical value (\eqn{TRUE} by default) indicating whether the variables should
-#'                be scaled to have unit variance. Alternatively, a vector of length equal the number of
-#'                columns of \eqn{A} can be supplied. The value is passed to scale.
+#' @param scale   bool, optional; \cr
+#'                logical value which indicates whether the variables should
+#'                be scaled to have unit variance (\eqn{TRUE} by default).
 #'
-#' @param retx    Bool (\eqn{TRUE}, \eqn{FALSE}), optional \cr
-#'                A logical value (\eqn{TRUE} by default) indicating whether the rotated variables / scores
-#'                should be returned.
+#' @param retx    bool, optional; \cr
+#'                logical value indicating whether the rotated variables / scores
+#'                should be returned (\eqn{TRUE} by default).
 #'
-#' @param p       Int, optional. \cr
-#'                Oversampling parameter for \eqn{rsvd}  (default \eqn{p=10}), see \code{\link{rsvd}}.
+#' @param p       integer, optional; \cr
+#'                oversampling parameter for \eqn{rsvd} (default \eqn{p=10}), see \code{\link{rsvd}}.
 #'
-#' @param q       Int, optional. \cr
-#'                Number of power iterations  for \eqn{rsvd} (default \eqn{q=1}), see \code{\link{rsvd}}.
+#' @param q       integer, optional; \cr
+#'                number of additional power iterations for \eqn{rsvd} (default \eqn{q=1}), see \code{\link{rsvd}}.
 #'
-#' @param rand  Bool (\eqn{TRUE}, \eqn{FALSE}). \cr
-#'              If (\eqn{TRUE}), a probabilistic strategy is used, otherwise a deterministic algorithm is used.
+#' @param rand   bool, optional; \cr
+#'               if (\eqn{TRUE}), the \eqn{rsvd} routine is used, otherwise \eqn{svd} is used.
 #'
-#' @param ................. .
 #'
 #' @return \code{rpca} returns a list with class \eqn{rpca} containing the following components:
-#'    \item{rotation}{  Array_like. \cr
-#'                      Matrix containing the rotation (eigenvectors); array with dimensions \eqn{(n, k)}.
+#'    \item{rotation}{  array_like; \cr
+#'                      the rotation (eigenvectors); \eqn{(n, k)} dimensional array.
 #'    }
 #'
-#'    \item{eigvals}{  Array_like. \cr
-#'                     The eigenvalues; 1-d array of length \eqn{k}.
+#'    \item{eigvals}{  array_like; \cr
+#'                     eigenvalues; \eqn{k} dimensional vector.
 #'    }
-#'    \item{sdev}{     Array_like \cr
-#'                     The standard deviations of the principal components.
+#'    \item{sdev}{     array_like; \cr
+#'                     standard deviations of the principal components; \eqn{k} dimensional vector.
 #'    }
-#'    \item{x}{        Array_like \cr
-#'                     If \eqn{retx} is true a matrix containing the scores / rotated data
-#'                     (centred and scaled if requested) is returned.
+#'    \item{x}{        array_like; \cr
+#'                     the scores / rotated data; \eqn{(m, k)} dimensional array.
+
 #'    }
-#'    \item{center, scale}{  Array_like .\cr
-#'                     The centering and scaling used, or \eqn{FALSE}.
+#'    \item{center, scale}{  array_like; \cr
+#'                     the centering and scaling used.
 #'    }
-#'    \item{.................}{.}
 #'
 #' @note  The principal components are not unique and only defined up to sign
 #' (a constant of modulus one in the complex case) and so may differ between different
 #'  PCA implementations.
 #'
 #' Similar to \code{\link{prcomp}} the variances are computed with the usual divisor N - 1.
-#'
-#' Note, that \eqn{scale = TRUE} cannot be used, if some columns have only constant entries.
 #'
 #'
 #' @author N. Benjamin Erichson, \email{erichson@uw.edu}
@@ -127,126 +110,121 @@
 #'print(iris.rpca) # Prints the rotations
 #'plot(iris.rpca) # Produce screeplot, variable and individuls factor maps.
 #'
-#'
-#'# You can compare the results with prcomp
-#'# iris.pca <- prcomp(log.iris, center = TRUE, scale. = TRUE)
-#'# summary(iris.pca) # Summary
-#'# print(iris.pca) # Prints the rotations
 
 #' @export
 rpca <- function(A, k=NULL, center=TRUE, scale=TRUE, retx=TRUE, p=10, q=2, rand = TRUE) UseMethod("rpca")
 
 #' @export
 rpca.default <- function(A, k=NULL, center=TRUE, scale=TRUE, retx=TRUE, p=10, q=2, rand = TRUE) {
-
-    A <- as.matrix(A)
   
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Checks
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if (any(is.na(A))) {
-      warning("Missing values are omitted: na.omit(A).")
-      A <- stats::na.omit(A)
-    }   
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Init rpca object
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    rpcaObj = list(rotation = NULL,
-                   eigvals = NULL,
-                   sdev = NULL,
-                   var = NULL,
-                   center = center,
-                   scale = scale,
-                   x=NULL)
-
-    m <- nrow(A)
-    n <- ncol(A)
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Set target rank
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if(is.null(k)) rand <- FALSE
-    if(is.null(k)) k <- min(n,m)
-    if(k > min(n,m)) k <- min(n,m)
-    if(k<1) stop("Target rank is not valid!")
-
-   
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Center/Scale data
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if(center == TRUE) {
-      rpcaObj$center <- colMeans(A)
-      A <- sweep(A, MARGIN = 2, STATS = rpcaObj$center, FUN = "-", check.margin = TRUE)
-      #A <- H(H(A) - rpcaObj$center)
-    } else { rpcaObj$center <- FALSE }
-    
-    if(scale == TRUE) {
-      rpcaObj$scale <- sqrt(colSums(A**2) / (m-1))
-      if(is.complex(rpcaObj$scale)) { rpcaObj$scale[Re(rpcaObj$scale) < 1e-8 ] <- 1+0i  
-      } else {rpcaObj$scale[rpcaObj$scale < 1e-8] <- 1}
-      A <- sweep(A, MARGIN = 2, STATS = rpcaObj$scale, FUN = "/", check.margin = TRUE)
-      #A <- H(H(A) / rpcaObj$scale)
-      #A[is.nan(A)] <- 0
-    } else { rpcaObj$scale <- FALSE }
-    
-    
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Compute randomized svd / eigen decomposition
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if(rand == TRUE) {
-      svdalg = 'rsvd'
-    }else { 
-      svdalg = 'svd' 
-    }
-
-    out <- switch(svdalg,
-                      svd = svd(A, nu = k, nv = k),
-                      rsvd = rsvd(A, k = k, p = p, q = q),
-                      stop("Selected SVD algorithm is not supported!")
-    )
-
-    rpcaObj$eigvals <- switch(svdalg,
-                              svd = out$d[1:k]**2 / (m-1),
-                              rsvd = out$d**2 / (m-1)
-    )
-
-    rpcaObj$rotation <- switch(svdalg,
-                              svd = out$v,
-                              rsvd = out$v
-    )
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Explained variance and explained variance ratio
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    rpcaObj$sdev <-  sqrt( rpcaObj$eigvals )
-    rpcaObj$var <- sum( apply( Re(A) , 2, stats::var ) )
-    if(is.complex(A)) rpcaObj$var <- Re(rpcaObj$var + sum( apply( Im(A) , 2, stats::var ) ))
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Add row and col names
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    rownames(rpcaObj$rotation) <- colnames(A)
-    colnames(rpcaObj$rotation) <- paste(rep('PC', k), 1:k, sep = "")
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Compute rotated data
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if(retx==TRUE) {
-      #rpcaObj$x <- A %*% rpcaObj$rotation # slow
-      #rpcaObj$x <- H(H(out$u[,1:k]) * out$d[1:k])
-      rpcaObj$x <- sweep(out$u[,1:k], MARGIN = 2, STATS = out$d[1:k], FUN = "*", check.margin = TRUE)
-      rownames(rpcaObj$x) <- rownames(A)
-    }
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Return
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    class(rpcaObj) <- "rpca"
-    return( rpcaObj )
-
+  A <- as.matrix(A)
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Checks
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (any(is.na(A))) {
+    warning("Missing values are omitted: na.omit(A).")
+    A <- stats::na.omit(A)
+  }   
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Init rpca object
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  rpcaObj = list(rotation = NULL,
+                 eigvals = NULL,
+                 sdev = NULL,
+                 var = NULL,
+                 center = center,
+                 scale = scale,
+                 x=NULL)
+  
+  m <- nrow(A)
+  n <- ncol(A)
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Set target rank
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(is.null(k)) rand <- FALSE
+  if(is.null(k)) k <- min(n,m)
+  if(k > min(n,m)) k <- min(n,m)
+  if(k<1) stop("Target rank is not valid!")
+  
+  
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Center/Scale data
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(center == TRUE) {
+    rpcaObj$center <- colMeans(A)
+    A <- sweep(A, MARGIN = 2, STATS = rpcaObj$center, FUN = "-", check.margin = TRUE)
+    #A <- H(H(A) - rpcaObj$center)
+  } else { rpcaObj$center <- FALSE }
+  
+  if(scale == TRUE) {
+    rpcaObj$scale <- sqrt(colSums(A**2) / (m-1))
+    if(is.complex(rpcaObj$scale)) { rpcaObj$scale[Re(rpcaObj$scale) < 1e-8 ] <- 1+0i  
+    } else {rpcaObj$scale[rpcaObj$scale < 1e-8] <- 1}
+    A <- sweep(A, MARGIN = 2, STATS = rpcaObj$scale, FUN = "/", check.margin = TRUE)
+    #A <- H(H(A) / rpcaObj$scale)
+    #A[is.nan(A)] <- 0
+  } else { rpcaObj$scale <- FALSE }
+  
+  
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Compute randomized svd / eigen decomposition
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(rand == TRUE) {
+    svdalg = 'rsvd'
+  }else { 
+    svdalg = 'svd' 
+  }
+  
+  out <- switch(svdalg,
+                svd = svd(A, nu = k, nv = k),
+                rsvd = rsvd(A, k = k, p = p, q = q),
+                stop("Selected SVD algorithm is not supported!")
+  )
+  
+  rpcaObj$eigvals <- switch(svdalg,
+                            svd = out$d[1:k]**2 / (m-1),
+                            rsvd = out$d**2 / (m-1)
+  )
+  
+  rpcaObj$rotation <- switch(svdalg,
+                             svd = out$v,
+                             rsvd = out$v
+  )
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Explained variance and explained variance ratio
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  rpcaObj$sdev <-  sqrt( rpcaObj$eigvals )
+  rpcaObj$var <- sum( apply( Re(A) , 2, stats::var ) )
+  if(is.complex(A)) rpcaObj$var <- Re(rpcaObj$var + sum( apply( Im(A) , 2, stats::var ) ))
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Add row and col names
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  rownames(rpcaObj$rotation) <- colnames(A)
+  colnames(rpcaObj$rotation) <- paste(rep('PC', k), 1:k, sep = "")
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Compute rotated data
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(retx==TRUE) {
+    #rpcaObj$x <- A %*% rpcaObj$rotation # slow
+    #rpcaObj$x <- H(H(out$u[,1:k]) * out$d[1:k])
+    rpcaObj$x <- sweep(out$u[,1:k], MARGIN = 2, STATS = out$d[1:k], FUN = "*", check.margin = TRUE)
+    rownames(rpcaObj$x) <- rownames(A)
+  }
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Return
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  class(rpcaObj) <- "rpca"
+  return( rpcaObj )
+  
 }#End rPCA
 
 
@@ -272,21 +250,21 @@ summary.rpca <- function( object , ... )
   variance = object$sdev**2
   explained_variance_ratio = variance / object$var
   cum_explained_variance_ratio = cumsum( explained_variance_ratio )
-
+  
   x <- t(data.frame( var = round(variance, 3),
-                              sdev = round(object$sdev, 3),
-                              prob = round(explained_variance_ratio, 3),
-                              cum = round(cum_explained_variance_ratio, 3)))
-
+                     sdev = round(object$sdev, 3),
+                     prob = round(explained_variance_ratio, 3),
+                     cum = round(cum_explained_variance_ratio, 3)))
+  
   rownames( x ) <- c( 'Explained variance',
                       'Standard deviations',
                       'Proportion of variance',
                       'Cumulative proportion')
-
+  
   colnames( x ) <- paste(rep('PC', length(object$sdev)), 1:length(object$sdev), sep = "")
-
+  
   x <- as.matrix(x)
-
+  
   return( x )
 }
 
@@ -323,3 +301,4 @@ predict.rpca <- function( object, newdata, ...)
   
   return( x )
 }
+
--- tests/testthat/test_rrpca.R
+++ tests/testthat/test_rrpca.R
@@ -24,35 +24,37 @@ atol_float64 <- 1e-8
 m = 50
 n = 40
 k = 5
-L <- matrix(runif(m*k), m, k)
-L <- L %*% t(L)
-L <- L[,1:n]
-S = rep(0, m*n)
-S[sample(0:(m*n), 20)] = 10
-S = matrix(S, m, n)
+L1 = matrix(rnorm(m*k), nrow = m, ncol = k) 
+L2 = matrix(rnorm(n*k), nrow = k, ncol = n) 
+L = L1 %*% L2
+S = matrix(runif(m*n, -500, 500), nrow = m, ncol = n)
+p = 0.1 # Percentage of sparse errors
+S = S * matrix(rbinom(m*n, size=1, prob=p), nrow = m, ncol = n)
 testMat = L + S
 
 
 #*************************************************************************************
 # Test 1: deterministic SVD
 #*************************************************************************************
-taurusRRPCA <- rrpca(testMat, k=1, maxiter=100,  tol=1e-08, trace=FALSE, rand=FALSE)
-nrmse <- sqrt(sum((L - taurusRRPCA$L)**2) / sum(L**2)) * 100 # reconstruction error
+out <- rrpca(testMat, maxiter=100, tol=1e-09, trace=FALSE, rand=FALSE)
+rerror <- norm(L - out$L, 'F') / norm(L, 'F')
+
 testthat::test_that("Test 1: RPCA", {
-  testthat::expect_equal(taurusRRPCA$L, L, tolerance = 1e-02)
-  testthat::expect_equal(taurusRRPCA$S, S, tolerance = 1e-02)
-  testthat::expect_lt(nrmse, 0.1)
+  testthat::expect_equal(out$L, L, tolerance = 1e-02)
+  testthat::expect_equal(out$S, S, tolerance = 1e-02)
+  testthat::expect_lt(rerror, 0.1)
 })
 
 #*************************************************************************************
 # Test 2: randomized SVD
 #*************************************************************************************
-taurusRRPCA <- rrpca(testMat, k=1, p=p, q=q, maxiter=100,  tol=1e-08, trace=FALSE)
-nrmse <- sqrt(sum((L - taurusRRPCA$L)**2) / sum(L**2)) * 100 # reconstruction error
+out <- rrpca(testMat, p=p, q=q, maxiter=100,  tol=1e-08, trace=FALSE)
+rerror <- norm(L - out$L, 'F') / norm(L, 'F')
+
 testthat::test_that("Test 2: Randomized RPCA", {
-  testthat::expect_equal(taurusRRPCA$L, L, tolerance = 1e-02)
-  testthat::expect_equal(taurusRRPCA$S, S, tolerance = 1e-02)
-  testthat::expect_lt(nrmse, 0.1)
+  testthat::expect_equal(out$L, L, tolerance = 1e-02)
+  testthat::expect_equal(out$S, S, tolerance = 1e-02)
+  testthat::expect_lt(rerror, 0.1)
 })
 
 #*************************************************************************************
@@ -71,21 +73,23 @@ testMat = L + S
 #*************************************************************************************
 # Test 3: deterministtic SVD
 #*************************************************************************************
-taurusRRPCA <- rrpca(testMat, k=1, maxiter=100,  tol=1e-08, trace=FALSE, rand=FALSE)
-nrmse <- sqrt(sum((L - taurusRRPCA$L)**2) / sum(L**2)) * 100 # reconstruction error
+out <- rrpca(testMat, maxiter=100, tol=1e-08, trace=FALSE, rand=FALSE)
+rerror <- norm(L - out$L, 'F') / norm(L, 'F')
+
 testthat::test_that("Test 3: RPCA", {
-  testthat::expect_equal(taurusRRPCA$L, L, tolerance = 1e-02)
-  testthat::expect_equal(taurusRRPCA$S, S, tolerance = 1e-02)
-  testthat::expect_lt(nrmse, 1)
+  testthat::expect_equal(out$L, L, tolerance = 1e-02)
+  testthat::expect_equal(out$S, S, tolerance = 1e-02)
+  testthat::expect_lt(rerror, 1)
 })
 
 #*************************************************************************************
 # Test 4: randomized SVD
 #*************************************************************************************
-taurusRRPCA <- rrpca(testMat, k=1, p=p, q=q, maxiter=100,  tol=1e-08, trace=FALSE)
-nrmse <- sqrt(sum((L - taurusRRPCA$L)**2) / sum(L**2)) * 100 # reconstruction error
+out <- rrpca(testMat, p=p, q=q, maxiter=100,  tol=1e-08, trace=FALSE)
+rerror <- norm(L - out$L, 'F') / norm(L, 'F')
+
 testthat::test_that("Test 4: Randomized RPCA", {
-  testthat::expect_equal(taurusRRPCA$L, L, tolerance = 1e-02)
-  testthat::expect_equal(taurusRRPCA$S, S, tolerance = 1e-02)
-  testthat::expect_lt(nrmse, 1)
+  testthat::expect_equal(out$L, L, tolerance = 1e-02)
+  testthat::expect_equal(out$S, S, tolerance = 1e-02)
+  testthat::expect_lt(rerror, 1)
 })
